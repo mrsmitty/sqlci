e63ea69e3f2e0c1ab3513ed557fc39cbaac33c85
Paul Smithdale <8188140+mrsmitty@users.noreply.github.com>
2021-04-01 09:11:53 +0800

    verbose

--- pipelines/Get-SqlPackage.ps1
+++ pipelines/Get-SqlPackage.ps1
@@ -1,6 +1,14 @@
+
 mkdir SqlPackage
+Write-Host "Downloading latest version of SQL Package"
 $sqlPackageUrl = "https://go.microsoft.com/fwlink/?linkid=2157302"
 Invoke-WebRequest -Uri $sqlPackageUrl -OutFile sqlPackage.zip
+
+Write-Host "Unpacking SQL Package"
 Expand-Archive sqlPackage.zip -DestinationPath SqlPackage
+
+Write-Host "Adding $(Get-Location)\SqlPackage to Path"
 $env:Path += ";$(Get-Location)\SqlPackage" 
+
+Write-Host "removing sqlpackage.zip"
 Remove-Item .\sqlPackage.zip
